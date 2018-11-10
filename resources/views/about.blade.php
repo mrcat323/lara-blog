e6a929bcab71c95b024bafe452d60dd317b7fb22
Mr. CaT <mrcat323@gmail.com>
2018-11-10 21:25:28 +0500

    some modifications in pages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 @section('content')
 <div class="jumbotron">
-	<h3>About Us</h3>
+	<h3 class="text-center">About Us</h3>
+	<h3>It's Lara-Blog, the blog CMS created with Laravel - Powerful PHP Framework by Mr. CaT</h3>
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,6 @@
 @section('content')
 <div class="jumbotron text-center">
 	<h2>Welcome to our Website!</h2>
+	<h3 class="text-center">It's Lara-Blog, the blog CMS by Mr. CaT</h3>
 </div>
 @endsection
